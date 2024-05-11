1a9828e4f2d30279acdf6416df9d3c66b9c0af7b
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-05-12 01:31:39 +0500

    right side bar and all pages dummy added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Kamil" };
+  const loggedIn = {
+    $id: "string",
+    email: "kamilraza@gmail.com",
+    userId: "string",
+    dwollaCustomerUrl: "string",
+    dwollaCustomerId: "string",
+    firstName: "Kamil",
+    lastName: "Raza",
+    address1: "string",
+    city: "string",
+    state: "string",
+    postalCode: "string",
+    dateOfBirth: "string",
+    ssn: "string",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +36,10 @@ const Home = () => {
             totalCurrentBalance={12500.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
