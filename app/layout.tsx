0636bc8c1a779d92aae31c6106f9be0ca9f43207
M Kamil Raza <125785123+kamilrazadev@users.noreply.github.com>
2024-05-21 21:38:29 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: "KamPay Banking",
   description: "An ultimate modern banking platform for everyone",
   icons: {
-    icon: "/icons/logo.svg",
+    icon: "/icons/logo.webp",
   },
 };
 
