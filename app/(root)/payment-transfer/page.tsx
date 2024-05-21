cf8a704fa2794dab0155d07923a135a306f84e92
M Kamil Raza <125785123+kamilrazadev@users.noreply.github.com>
2024-05-21 21:22:29 +0500

    Update page.tsx

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+export const metadata = {
+  title: "Payment Transfer | KamPay Banking",
+  description: "An ultimate modern banking platform for everyone",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 const PaymentTransfer = () => {
   return <div>PaymentTransfer</div>;
 };
