2a893443e9d838fa9015319b44a4238970a3e320
M Kamil Raza <125785123+kamilrazadev@users.noreply.github.com>
2024-05-21 21:21:54 +0500

    Update page.tsx

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+export const metadata = {
+  title: "My Banks | KamPay Banking",
+  description: "An ultimate modern banking platform for everyone",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 const MyBanks = () => {
   return <div>MyBanks</div>;
 };
