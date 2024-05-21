7b05d77c53445535a64f4cc4063b0305ad02b5e0
M Kamil Raza <125785123+kamilrazadev@users.noreply.github.com>
2024-05-21 21:22:59 +0500

    Update page.tsx

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+export const metadata = {
+  title: "Transaction History | KamPay Banking",
+  description: "An ultimate modern banking platform for everyone",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
 const TransactionHistory = () => {
   return <div>TransactionHistory</div>;
 };
