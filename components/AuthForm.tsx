1f32797de869b5ed5fb0d24e7d59d4fbab18cb46
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-05-26 02:44:19 +0500

    appwrite keys added

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,8 +19,11 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,13 +39,27 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
-  }
+
+    try {
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -96,6 +113,13 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city name"
                   />
 
                   <div className="flex gap-4">
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.error("Error in sign in", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+  } catch (error) {
+    console.error("Error in sign up", error);
+  }
+};
