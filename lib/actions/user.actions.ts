f9686dc9d92de0b62fd8b64bd276ef9c20146207
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-05-26 15:01:17 +0500

    updated userr.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
   try {
   } catch (error) {
@@ -8,7 +13,26 @@ export const signIn = async () => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, lastName, firstName } = userData;
   try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("my-custom-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error in sign up", error);
   }
